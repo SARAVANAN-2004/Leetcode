b374972e761d66d3254130f25d6a5069938efcdf
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-15 09:22:33 +0530

    Time: 69 ms (24.55%), Space: 45.2 MB (65.12%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        List<Integer> arr = new ArrayList<>();
+        String vowel = "aeiou";
+        Map<Character,Integer> char_map  = new  HashMap<>();
+        for(int i = 0;i<5;i++){
+            char_map.put(vowel.charAt(i),(int)Math.pow(2,i));
+        }
+        System.out.println(char_map);
+        Map<Integer,Integer> map = new HashMap<>();
+        int sub = 0;
+        int prexor = 0;
+        map.put(0,-1);
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(char_map.containsKey(ch)){
+                prexor ^= char_map.get(ch);
+            }
+            if(!map.containsKey(prexor)){
+                map.put(prexor,i);
+            }else{
+                sub = Math.max(sub,i - map.get(prexor));
+            }
+        }
+        return sub;
+    }
+}
