882bda3db438dfc7fff6507115379ddbddd25682
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-19 14:35:13 +0530

    Time: 2 ms (68.85%), Space: 43 MB (15.13%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String exp) {
         List<Integer> result = new ArrayList<>();
-        if(exp.length() == 0) return result;
-
-        if(exp.length() == 1){
+        if(exp.length() == 0){
+            return result;
+        }
+        if(exp.length() == 1 || (exp.length() == 2 && Character.isDigit(exp.charAt(0)))){
             result.add(Integer.valueOf(exp));
             return result;
         }
-        if (exp.length() == 2 && Character.isDigit(exp.charAt(0))) {
-            result.add(Integer.parseInt(exp));
-            return result;
-        }
-
         for(int i = 0;i<exp.length();i++){
             char curr = exp.charAt(i);
             if(Character.isDigit(curr)){
@@ -19,9 +15,8 @@ class Solution {
             }
             List<Integer> leftres = diffWaysToCompute(exp.substring(0,i));
             List<Integer> rightres = diffWaysToCompute(exp.substring(i+1));
-
             for(int left:leftres){
-                for(int right:rightres){
+                for(int right: rightres){
                     int computedres = 0;
                     switch(curr){
                         case '+':
@@ -32,11 +27,12 @@ class Solution {
                             break;
                         case '*':
                             computedres = left * right;
-                            break;  
+                            break;
                     }
                     result.add(computedres);
                 }
             }
+            
         }
         return result;
     }
