b8e71dbc10897963bf563a59aaa4ea1f7781452a
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-20 21:26:24 +0530

    Time: 27 ms (44.93%), Space: 45.8 MB (5.16%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        String revstr = new StringBuilder(s).reverse().toString();
+        String pattern = s + "*"+  revstr;
+        int[] lps = kmpalgo(pattern);
+        System.out.println(Arrays.toString(lps));
+        int n = lps[pattern.length()-1];
+        return new StringBuilder(s.substring(n)).reverse().toString()+s;
+    }
+
+    public int[] kmpalgo(String pattern){
+        int[] lps = new int[pattern.length()];
+        int i = 0;
+        int j = 1;
+        while(j < pattern.length()){
+            if(pattern.charAt(i) == pattern.charAt(j)){
+                lps[j] = i+1;
+                i++;
+                j++;
+            }else{
+                if(i != 0){
+                    i = lps[i-1];
+                }else{
+                    lps[j] = i;
+                    j++;
+                }
+            }
+        }
+        return lps;
+    }
+}
