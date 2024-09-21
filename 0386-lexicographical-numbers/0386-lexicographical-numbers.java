bf878b0353e262967ed55d0b8a81d0750deb870c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-21 07:30:26 +0530

    Time: 70 ms (5.13%), Space: 53.2 MB (5.13%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,11 +1,10 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        String[] s = new String[n];
         TreeMap<String,Integer> map = new TreeMap<>();
         List<Integer> ans = new ArrayList<>();
         for(int i  = 0;i<n;i++){
-            s[i] = (i+1)+"";
-            map.put(s[i],i+1);
+            String a = (i+1)+"";
+            map.put(a,i+1);
         }
         for(String a:map.keySet()){
             ans.add(map.get(a));
