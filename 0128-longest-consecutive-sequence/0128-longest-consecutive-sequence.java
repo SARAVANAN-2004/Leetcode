0d7a4563ca56c022df5a40dd9c3c47c2b8c35679
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-23 23:58:33 +0530

    Time: 77 ms (35.08%), Space: 83.3 MB (5.99%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,36 @@
+class obj{
+    int cnt;
+    boolean visited;
+    public obj(int cnt,boolean visited){
+        this.cnt = cnt;
+        this.visited = visited;
+    }
+}
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer,obj> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,new obj(0,false));
+        }
+        int ans = 0;
+        for(int i:nums){
+            
+            dfs(map,i,i);
+            
+            ans = Math.max(ans,map.get(i).cnt);
+        }
+        return ans;
+    }
+
+    public void dfs(Map<Integer,obj> map,int num,int curnum){
+        if(!map.containsKey(curnum) || map.get(curnum).visited){
+            return;
+        }
+        map.get(num).cnt += 1;
+        map.get(curnum).visited = true;
+        dfs(map,num,curnum-1);
+        dfs(map,num,curnum+1);
+
+    }
+}
