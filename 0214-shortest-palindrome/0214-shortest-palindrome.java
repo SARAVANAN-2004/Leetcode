5bceeb80f81da419d023cf842c8d76f47373e973
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-26 13:40:52 +0530

    Time: 166 ms (26.48%), Space: 45.7 MB (5.14%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,28 +1,26 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        String revstr = new StringBuilder(s).reverse().toString();
-        String pattern = s + "*"+  revstr;
-        int[] lps = kmpalgo(pattern);
-        // System.out.println(Arrays.toString(lps));
-        int n = lps[pattern.length()-1];
-        return new StringBuilder(s.substring(n)).reverse().toString()+s;
+        StringBuilder str = new StringBuilder(s);
+        str.reverse();
+        int[] lps = generatelps(s+"-"+str.toString());
+        System.out.println(Arrays.toString(lps));
+        return new StringBuilder(s.substring(lps[(2*s.length())])).reverse().toString() + s;
     }
-
-    public int[] kmpalgo(String pattern){
-        int[] lps = new int[pattern.length()];
-        int i = 0;
-        int j = 1;
-        while(j < pattern.length()){
-            if(pattern.charAt(i) == pattern.charAt(j)){
-                lps[j] = i+1;
+    public int[] generatelps(String s){
+        int[] lps = new int[s.length()];
+        lps[0] = 0;
+        int streakidx = 0;
+        for(int i = 1;i<s.length();){
+            if(s.charAt(i) == s.charAt(streakidx)){
+                streakidx++;
+                lps[i] = streakidx;
                 i++;
-                j++;
             }else{
-                if(i != 0){
-                    i = lps[i-1];
+                if(streakidx == 0){
+                    lps[i] = 0;
+                    i++;
                 }else{
-                    lps[j] = i;
-                    j++;
+                    streakidx = lps[streakidx-1];
                 }
             }
         }
