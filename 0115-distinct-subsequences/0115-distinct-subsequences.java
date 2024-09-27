1023efd4b16dfed14635bbd344ac14fb61c89e23
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-27 09:09:03 +0530

    Time: 19 ms (8.36%), Space: 50.9 MB (10.81%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,23 +4,23 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
-        return solve(dp,s,t,0,0);
+        return solve(dp,s,t,s.length()-1,t.length()-1);
     }
     int solve(int[][] dp,String s,String t,int row,int col){
-        if (col == t.length()){
+        if (col < 0){
             return 1;
         }
-        if(row ==  s.length()){
+        if(row < 0){
             return 0;
         }
         if(dp[row][col] != -1){
             return dp[row][col];
         }
         if(s.charAt(row) == t.charAt(col)){
-            return dp[row][col] = solve(dp,s,t,row+1,col+1) + solve(dp,s,t,row+1,col);
+            dp[row][col] = solve(dp,s,t,row-1,col-1) + solve(dp,s,t,row-1,col);
+        }else{
+            dp[row][col] = solve(dp,s,t,row-1,col);
         }
-        return dp[row][col] = solve(dp,s,t,row+1,col);
-        
-    
+        return dp[row][col];
     }
 }
