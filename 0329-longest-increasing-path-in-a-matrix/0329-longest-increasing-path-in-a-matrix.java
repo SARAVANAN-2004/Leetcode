5903c64f22dafb46f48e99c3dcd84d3c51352f8c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-09-27 11:40:34 +0530

    Time: 8 ms (83.46%), Space: 46.1 MB (5.21%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -6,22 +6,23 @@ class Solution {
         for(int[] rows:dp){
             Arrays.fill(rows,-1);
         }
-
+        int max = 0;
         for(int r = 0;r < row;r++){
             for(int c = 0;c <col;c++){
                 if(dp[r][c] == -1){
                      dp[r][c] = dfs(matrix,dp,row,col,r,c);
                 }
-            }
-        }
-        int max = 0;
-        for(int r = 0;r < row;r++){
-            for(int c = 0;c <col;c++){
-                // System.out.print(dp[r][c]+" ");
                 max = Math.max(max,dp[r][c]);
             }
-            // System.out.println();
         }
+        
+        // for(int r = 0;r < row;r++){
+        //     for(int c = 0;c <col;c++){
+        //         // System.out.print(dp[r][c]+" ");
+                
+        //     }
+        //     // System.out.println();
+        // }
         return max+1;
     }
     int  dfs(int[][] matrix,int [][] dp,int row,int col,int r,int c){
