bac63f3d86c013955e11e38d41992ef492cb2b29
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-04 23:59:36 +0530

    Time: 22 ms (13.92%), Space: 51.4 MB (5.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        int l = 0,r = s.length()-1;
+        System.out.println(s);
+        return solve(s.trim().toLowerCase(),l,r);
+    }
+    public boolean solve(String s,int l,int r){
+        if(l>=r){
+            return true;
+        }
+        char ch1 = s.charAt(l);
+        char ch2 = s.charAt(r);
+
+        if((valid(ch1) && valid(ch2)) && (ch1 != ch2)){
+            // System.out.println(ch1+" "+ch2);
+            return false;
+        }
+        else if(!valid(ch1) && valid(ch2)){
+            return solve(s,l+1,r);
+        }else if(valid(ch1) && !valid(ch2)){
+            return solve(s,l,r-1);
+        }
+
+        return solve(s,l+1,r-1);
+        
+        
+    }
+
+    boolean valid(char ch){
+        if(((ch >= 'A' && ch <= 'Z')|| ( ch >= 'a' && ch <= 'z')) || Character.isDigit(ch) ){
+            return true;
+        }
+        return false;
+    }
+}
