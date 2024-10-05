c1440ca5d7cee743a227cdfe7bcf71f95a34a437
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-05 11:14:46 +0530

    Time: 137 ms (16.17%), Space: 44.6 MB (28.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,16 +1,31 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        s1 = sort(s1);
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+        int l = s1.length();
+        int[] arr1 = new int[26];
+        fill(arr1,s1);
+    
+        for(int i = 0;i< s2.length()-l+1;i++){
+            int[] arr2 = new int[26];
+            fill(arr2,s2.substring(i,i+l));
+            if(valid(arr1,arr2,s1)){
                 return true;
+            }
         }
         return false;
     }
-    
-    public String sort(String s) {
-        char[] t = s.toCharArray();
-        Arrays.sort(t);
-        return new String(t);
+    boolean valid(int[] arr1,int[] arr2,String s){
+        for(char ch:s.toCharArray()){
+            if(arr1[ch-'a'] != arr2[ch-'a']){
+                return false;
+            }
+        }
+        return true;
     }
+
+    void fill(int[] arr,String s){
+        for(char ch:s.toCharArray()){
+            arr[ch-'a'] += 1;
+        }
+    }
+    
 }
