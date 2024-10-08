e0fad41a665a1eefe3b293be6ebce82d8557f2b9
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-08 14:46:49 +0530

    Time: 88 ms (25.42%), Space: 54.9 MB (55.89%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,19 +1,19 @@
 class Solution {
+
     public int minSwaps(String s) {
-
-        
-        Stack<Character> stack = new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(stack.isEmpty()){
-                stack.push(ch);
-            }else if(stack.peek() == '[' && ch == ']'){
-                stack.pop();
-            }else{
-                stack.push(ch);
+        Stack<Character> stack = new Stack();
+        int unbalanced = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            // If an opening bracket is encountered, push it in the stack.
+            if (ch == '[') stack.push(ch);
+            else {
+                // If the stack is not empty, pop it.
+                if (!stack.isEmpty()) stack.pop();
+                // Otherwise increase the count of unbalanced brackets.
+                else unbalanced++;
             }
-            
         }
-
-        return ((stack.size()/2)+1)/2;
+        return (unbalanced + 1) / 2;
     }
 }
