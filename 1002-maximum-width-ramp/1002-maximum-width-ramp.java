55052237e4d2a440ea914c612be148479867778d
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-10 19:44:04 +0530

    Time: 28 ms (33.83%), Space: 56.5 MB (5.72%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -1,29 +1,19 @@
 class Solution {
-
     public int maxWidthRamp(int[] nums) {
         int n = nums.length;
-        Stack<Integer> indicesStack = new Stack<>();
-
-        // Fill the stack with indices in increasing order of their values
-        for (int i = 0; i < n; i++) {
-            if (indicesStack.isEmpty() || nums[indicesStack.peek()] > nums[i]) {
-                indicesStack.push(i);
+        Stack<Integer> stack = new Stack<>();
+        for(int  i = 0;i<n;i++){
+            if(stack.isEmpty() || nums[stack.peek()] >= nums[i]){
+                stack.push(i);
             }
         }
 
-        int maxWidth = 0;
-
-        // Traverse the array from the end to the start
-        for (int j = n - 1; j >= 0; j--) {
-            while (
-                !indicesStack.isEmpty() && nums[indicesStack.peek()] <= nums[j]
-            ) {
-                maxWidth = Math.max(maxWidth, j - indicesStack.peek());
-                // Pop the index since it's already processed
-                indicesStack.pop();
+        int max = 0;
+        for(int j = n-1;j >= 0;j--){
+            while(!stack.isEmpty() && nums[stack.peek()] <= nums[j]){
+                max = Math.max(max,j - stack.pop());
             }
         }
-
-        return maxWidth;
+        return max;
     }
 }
