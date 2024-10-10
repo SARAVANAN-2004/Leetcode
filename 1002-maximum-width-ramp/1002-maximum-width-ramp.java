ad05be770a5dde733f4d6668054e85158cc5c278
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-10 19:09:49 +0530

    Time: 4 ms (84.83%), Space: 55.6 MB (20.65%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -1,19 +1,21 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        Stack<Integer> stack = new Stack<>();
+        int n = nums.length;
+        int[] indices = new int[n];
         int maxWidth = 0;
 
-        // Build a stack of decreasing elements' indices
-        for (int i = 0; i < nums.length; i++) {
-            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
-                stack.push(i);
+        // Step 1: Build an array of indices where nums[indices] are decreasing
+        int indexSize = 0; // This will act like a stack pointer for our indices array
+        for (int i = 0; i < n; i++) {
+            if (indexSize == 0 || nums[indices[indexSize - 1]] > nums[i]) {
+                indices[indexSize++] = i;
             }
         }
 
-        // Traverse the array from the end to find the maximum width
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
-                maxWidth = Math.max(maxWidth, i - stack.pop());
+        // Step 2: Traverse from the end of the array and check for maximum ramp
+        for (int i = n - 1; i >= 0; i--) {
+            while (indexSize > 0 && nums[indices[indexSize - 1]] <= nums[i]) {
+                maxWidth = Math.max(maxWidth, i - indices[--indexSize]);
             }
         }
 
