79c323615dc4ef3119d087670c44d3f09b544d7e
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-10-12 08:41:53 +0530

    Time: 27 ms (95.11%), Space: 94.3 MB (20.38%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -1,29 +1,26 @@
 class Solution {
     public int minGroups(int[][] intervals) {
         int n  = intervals.length;
-        List<int[]> arr = new ArrayList<>();
+        int rst = Integer.MIN_VALUE;
+        int rend = 0;
         for(int[] i : intervals){
-            arr.add(new int[]{i[0],1});
-            arr.add(new int[]{i[1],-1});
+            rst = Math.min(i[0],rst);
+            rend = Math.max(rend,i[1]);
         }
-        Collections.sort(arr,(a,b)->{
-            if(a[0] != b[0]){
-                return a[0] - b[0];
-            }
-            return b[1] - a[1];
-        });
-       
+        
+        int[] arr =new int[rend+2];
+        for(int[] i :intervals){
+            arr[i[0]]++;
+            arr[i[1] +1]--;
+        }
         int sum = 0;
         int max =0;
-        for(int i = 0;i<arr.size();i++){
-            sum += arr.get(i)[1];
-           
+        for(int i = 0;i<arr.length;i++){
+            sum += arr[i];
             max = Math.max(sum,max);
         }
-        // System.out.println(Arrays.toString(pre));
-        // for(int[] i :arr){
-        //     System.out.println(Arrays.toString(i));
-        // }
+        // System.out.println(Arrays.toString(arr));
+    
         return max;
     }
 }
