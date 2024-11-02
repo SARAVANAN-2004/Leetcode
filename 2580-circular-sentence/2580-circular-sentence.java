1597d3d492fdaae57a05cb712ab653a46046c7f7
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-11-02 09:03:37 +0530

    Time: 2 ms (18.09%), Space: 42.2 MB (12.59%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -7,10 +7,7 @@ class Solution {
 
     boolean solve(String[] arr){
         int n = arr.length;
-        // if(arr[0].charAt(0) != arr[n-1].charAt(arr[n-1].length()-1) || !check(arr[0])){
-        //     return false;
-        // }
-
+       
 
         for(int  i= 0;i<n;i++){
             int a = i+1;
@@ -18,7 +15,7 @@ class Solution {
                 a = 0;
             }
             // System.out.println(arr[i]+" "+arr[a]);
-            if(arr[a].charAt(0) != arr[i].charAt(arr[i].length()-1) || !check(arr[i])){         
+            if(arr[a].charAt(0) != arr[i].charAt(arr[i].length()-1)){         
             // System.out.println(i + " "+a);
             return false;
         }
@@ -27,13 +24,13 @@ class Solution {
 
         return true;
     }
-    boolean check(String s){
-        for(char ch:s.toCharArray()){
-            if(!Character.isLetter(ch)){
+    // boolean check(String s){
+    //     for(char ch:s.toCharArray()){
+    //         if(!Character.isLetter(ch)){
                 
-                return false;
-            }
-        }
-        return true;
-    }
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
