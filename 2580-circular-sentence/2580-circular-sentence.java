9d2a9a513ba22c23388f94ce8a19173fb4ce2fea
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-11-02 09:05:00 +0530

    Time: 1 ms (96.81%), Space: 41.7 MB (83.14%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,36 +1,16 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
         String[] arr = sentence.split(" ");
-
-        return solve(arr);
-    }
-
-    boolean solve(String[] arr){
         int n = arr.length;
-       
-
         for(int  i= 0;i<n;i++){
             int a = i+1;
             if(a == n){
                 a = 0;
             }
-            // System.out.println(arr[i]+" "+arr[a]);
             if(arr[a].charAt(0) != arr[i].charAt(arr[i].length()-1)){         
-            // System.out.println(i + " "+a);
             return false;
         }
         }
-
-
         return true;
     }
-    // boolean check(String s){
-    //     for(char ch:s.toCharArray()){
-    //         if(!Character.isLetter(ch)){
-                
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
 }
