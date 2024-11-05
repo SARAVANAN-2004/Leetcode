25d2f3392f9f7b40de25c1ca45c0d38b1d258824
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-11-05 13:22:19 +0530

    Time: 3 ms (97.78%), Space: 44.5 MB (85.94%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minChanges(String s) {
-        char[] arr = s.toCharArray();
+       
         int cnt  = 0;
-        for(int i = 0;i<arr.length;i+=2){
-            if(arr[i] != arr[i+1]){
+        for(int i = 0;i<s.length();i+=2){
+            if(s.charAt(i)!= s.charAt(i+1)){
                 cnt++;
             }
         }
