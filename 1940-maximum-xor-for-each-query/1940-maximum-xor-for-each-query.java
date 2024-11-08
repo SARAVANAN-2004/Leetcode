c428cbae49681c33e5af3c6952b552b3f686707c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-11-08 18:03:17 +0530

    Time: 3 ms (78.95%), Space: 58.6 MB (81.61%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
@@ -3,9 +3,10 @@ class Solution {
         int n = nums.length;
         int[] arr = new int[n];
         int pre = 0;
+        int k = ((int)(Math.pow(2,maximumBit))-1);
         for(int i = 0;i<n;i++){
             pre ^= nums[i];
-            arr[n-i-1] = pre^((int)(Math.pow(2,maximumBit))-1);
+            arr[n-i-1] = pre^k;
             
         }
 
