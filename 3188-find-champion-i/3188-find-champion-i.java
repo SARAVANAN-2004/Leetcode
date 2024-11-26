078666f4fb11b17a5e0551c19b41fc5e6992dd99
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-11-26 09:30:27 +0530

    Time: 6 ms (6.86%), Space: 45.8 MB (7.06%) - LeetHub

---
+++ 3188-find-champion-i/3188-find-champion-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findChampion(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[] arr = new int[row];
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                if(i!=j && grid[i][j] == 1){
+                    arr[i]++;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        int maxIdx = 0;
+        int max = 0;
+        for(int i = 0;i<row;i++){
+            if(max < arr[i]){
+                max = arr[i];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+}
