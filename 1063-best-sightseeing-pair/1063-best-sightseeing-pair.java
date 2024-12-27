64473dec79ee29999feabeee938a00a935983a68
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-12-27 09:02:55 +0530

    Time: 2 ms (100%), Space: 53.3 MB (61.35%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -4,8 +4,12 @@ class Solution {
         int maxSpot = val[0]+0;
         int score = 0,n = val.length;
         for(int i = 1;i<n;i++){
-            score = Math.max(score,maxSpot + (val[i] - i));
-            maxSpot = Math.max(maxSpot,val[i]+i);
+            if(score < maxSpot + (val[i] - i)){
+                score = maxSpot + (val[i] - i);
+            }
+            if(maxSpot < val[i]+i){
+            maxSpot = val[i]+i;
+            }
         }
         return score;
     }
