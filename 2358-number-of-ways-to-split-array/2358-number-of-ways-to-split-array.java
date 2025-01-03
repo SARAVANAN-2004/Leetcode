14ea55dcdd0533ad17d40437a598872b27463226
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-03 18:28:42 +0530

    Time: 4 ms (16.81%), Space: 60.9 MB (20.58%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        long[] prefix = new long[n];
+        long sum = 0;
+        for(int i = 0;i<n;i++){
+            sum += nums[i];
+            prefix[i] = sum;
+        }
+        int cnt = 0;
+        for(int i = 0;i<n-1;i++){
+            if(prefix[i] >= prefix[n-1] - prefix[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
