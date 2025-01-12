d8f98128e3250304b4e62a3cbe1f1523f7202bdc
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-12 09:58:35 +0530

    Time: 15 ms (83.4%), Space: 46 MB (28.63%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,24 +1,31 @@
 class Solution {
-   public boolean canBeValid(String s, String locked) {
-       int n = s.length();
-       int minOpen = 0, maxOpen = 0;
-       for (int i = 0; i < n; i++) {
-           if (locked.charAt(i) == '0') {
-               maxOpen++;
-               minOpen = minOpen == 0 ? 1 : minOpen - 1;
-           } else {
-               if (s.charAt(i) == '(') {
-                   minOpen++;
-                   maxOpen++;
-               } else {
-                   maxOpen--;
-                   minOpen = minOpen == 0 ? 1 : minOpen - 1;
-                   if (minOpen > maxOpen) {
-                       return false;
-                   }
-               }
-           }
-       }
-       return minOpen == 0;
-   }
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        
+        if (n % 2 != 0) {
+            return false;
+        }
+        
+        int balance = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance < 0) return false;
+        }
+        
+        balance = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance < 0) return false;
+        }
+        
+        return true;
+    }
 }
