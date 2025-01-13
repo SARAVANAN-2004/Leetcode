9b136a76ef2d822ec023fa6052a1254d20dcd6b2
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-13 12:06:27 +0530

    Time: 106 ms (45.4%), Space: 42.5 MB (72.52%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,28 +1,32 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] num = new int[n+2];
-        num[0] = num[n+1] = 1;
+        int[] arr = new int[n+2];
+        arr[0] = arr[n+1] = 1;
         for(int i = 0;i<n;i++){
-            num[i+1] = nums[i];
+            arr[i+1] = nums[i];
         }
-        n+=2;
-        int[][] dp = new int[n][n];
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        System.out.println(Arrays.toString(arr));
+
+        int[][] dp = new int[n+1][n+1];
+       for(int[] i:dp){
+            Arrays.fill(i,-1);
         }
-        return solve(num,dp,1,n-2);
+       int max = solve(arr,dp,1,n);
+        
+       return max;
     }
-    int solve(int[] arr,int[][] dp,int left,int right){
-        if(left > right){
-            return 0;
-        }
+    public int solve(int[] arr,int[][] dp,int left,int right){
+        if(left > right) return 0;
         if(dp[left][right] != -1){
             return dp[left][right];
         }
-        int max = -1;
+        int max = Integer.MIN_VALUE;
         for(int i = left;i<=right;i++){
-            max = Math.max(max,solve(arr,dp,left,i-1) + solve(arr,dp,i+1,right)+(arr[left-1]*arr[i]*arr[right+1]));
+            int cost = (arr[left-1] * arr[i] * arr[right+1]) + 
+            solve(arr,dp,left,i-1) + solve(arr,dp,i+1,right);
+            max = Math.max(cost,max);
+
         }
         dp[left][right] = max;
         return max;
