70b3a57fa53d4fed3ffce88620192b8cb42dca71
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-13 10:23:57 +0530

    Time: 8 ms (20.76%), Space: 45.5 MB (45.67%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,42 +1,34 @@
 class Solution {
-    public int[] asteroidCollision(int[] arr) {
+    public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        for(int i:arr){
-            if(stack.isEmpty() || valid(stack.peek(),i,stack)){
-                stack.push(i);
+        for(int n :asteroids){
+            if(!stack.isEmpty() && stack.peek() > 0 && n < 0){
+                boolean add = true;
+                while(!stack.isEmpty() && stack.peek() > 0 && n < 0){
+                    if(stack.peek() == Math.abs(n)){
+                        stack.pop();
+                        add = false;
+                        break;
+                    }else if(stack.peek() > Math.abs(n)){
+                        add = false;
+                        break;
+                    }else{
+                        stack.pop();
+                    }
+                }
+
+                if(add){
+                    stack.push(n);
+                }
             }else{
-                collision(stack,i);
-            }
-            
-        }
-
-        int[] ans = new int[stack.size()];
-        for(int i = stack.size()-1;i>=0 ;i--){
-            ans[i] = stack.pop();
-        }
-        return ans;
-    }
-    void collision(Stack<Integer> stack,int cur){
-
-        while(!stack.isEmpty() && !valid(stack.peek(),cur,stack)){
-            int ans = cur+stack.peek();
-            if(ans == 0){
-                stack.pop();
-                return;
-            }
-            if(ans > 0){
-                return;
-            }else{
-                stack.pop();
+                stack.push(n);
             }
         }
-        if(stack.isEmpty() || valid(stack.peek(),cur,stack)){
-            stack.push(cur);
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+        while(!stack.isEmpty()){
+            res[i--] = stack.pop();
         }
-    }
-
-    boolean valid(int a,int b,Stack<Integer> stack){
-        if(!stack.isEmpty() && (a>0 && b < 0)) return false;
-        return true;
+        return res;
     }
 }
