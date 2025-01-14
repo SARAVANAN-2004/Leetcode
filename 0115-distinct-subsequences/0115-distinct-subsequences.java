d603ded15e8f0f2261b673ddc62deae86edbb5d5
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-14 13:55:21 +0530

    Time: 17 ms (77.4%), Space: 51.1 MB (66.74%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,20 +1,22 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int n = s.length() , m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i[]:dp){
+            Arrays.fill(i,-1);
         }
-        return solve(s, t, n-1, m-1, dp);
+        return func(n-1,m-1,s,t,dp);
     }
-    private int solve(String s, String t, int i, int j, int[][] dp){
+    int func(int i,int j,String s,String t,int[][] dp){
         if(j < 0) return 1;
+
         if(i < 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
+
         if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = solve(s, t, i-1, j-1, dp) + solve(s, t, i-1, j, dp);
+            return dp[i][j] = func(i-1,j-1,s,t,dp) + func(i-1,j,s,t,dp);
         }
-        return dp[i][j] = solve(s, t, i-1, j, dp);
+        return dp[i][j] =  func(i-1,j,s,t, dp);
     }
+
 }
