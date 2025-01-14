7051cda63116c2483ae884cdbebd3946a25158ff
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-14 12:30:42 +0530

    Time: 1 ms (64.77%), Space: 42 MB (39.24%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -2,30 +2,27 @@ class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> res = new ArrayList<>();
         List<String> ans = new ArrayList<>();
-        int curwidth = 0;
-        int len = 0;
-        for(int i = 0;i<words.length;i++){
-            String s = words[i];
+        int curWidth = 0,len = 0;
+        for(String s:words){
             int curLen = s.length();
-            if(curwidth+curLen>maxWidth){
-                process(res,ans,maxWidth,curwidth,len);
-                curwidth = 0;
+            if(curLen + curWidth > maxWidth){
+                process(res,ans,curWidth,len,maxWidth);
+                curWidth = 0;
                 len = 0;
                 ans.clear();
             }
             len += s.length();
-            curwidth += s.length()+1;
+            curWidth += s.length()+1;
             ans.add(s);
         }
-        lastLine(res,ans,maxWidth,curwidth,len);
+        lastLine(res,ans,len,maxWidth);
         return res;
-        
     }
-    void lastLine(List<String> res,List<String> ans,int maxWidth,int curwidth,int len){
+    void lastLine(List<String> res,List<String> ans,int len,int maxWidth){
         StringBuilder temp = new StringBuilder();
         for(String s:ans){
             temp.append(s);
-            if(temp.length() <maxWidth){
+            if(temp.length() < maxWidth){
                 temp.append(" ");
             }
         }
@@ -34,38 +31,34 @@ class Solution {
         }
         res.add(temp.toString());
     }
-    
 
-    void process(List<String> res,List<String> ans,int maxWidth,int curwidth,int len){
+    void process(List<String> res,List<String> ans,int curWidth,int len,int maxWidth){
         StringBuilder temp = new StringBuilder();
-        int spaces = ans.size()-1;
-        // System.out.println(ans);
-        // System.out.println("max "+maxWidth+" curWidth "+ curwidth+" space "+spaces+"len "+len);
-        int extraspace = maxWidth - len;
-        int minspace = maxWidth - len;
-        int maxspace  = 0;
-        if(spaces != 0){
-        minspace = extraspace/spaces;
-        maxspace = extraspace%spaces;
-        }
-        // System.out.println("extra "+extraspace+" minspace "+minspace+" maxspace "+maxspace);
-
-        String space = "";
-        for(int i = 0;i<minspace;i++){
-            space += " ";
+        int space = ans.size() - 1;
+        int extraSpace = maxWidth - len;
+        int minSpace = extraSpace;
+        int maxSpace = 0;
+        if(space != 0){
+            minSpace = extraSpace / space;
+            maxSpace = extraSpace % space;
         }
 
-        for(int i = 0; i<ans.size();i++){
-            temp.append(ans.get(i));
+        String spaces = "";
+        for(int i = 0;i<minSpace;i++){
+            spaces += " ";
+        }
+        for(String s:ans){
+          
+            temp.append(s);
             if(temp.length() < maxWidth){
-                temp.append(space);
-                if(maxspace>0){
+                temp.append(spaces);
+                if(maxSpace > 0){
                     temp.append(" ");
-                    maxspace--;
+                    maxSpace--;
                 }
             }
         }
+     
         res.add(temp.toString());
-        
     }
 }
