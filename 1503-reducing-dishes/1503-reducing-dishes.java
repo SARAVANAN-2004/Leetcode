27fe24375f7657d94ddbce7a849075c2dec4d140
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-26 21:03:35 +0530

    Time: 8 ms (50.23%), Space: 45.8 MB (14.32%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.java
+++ 1503-reducing-dishes/1503-reducing-dishes.java
@@ -4,10 +4,10 @@ class Solution {
         int n = arr.length;
         int[][] dp = new int[n+1][n+1];
         int maxVal = 0;
-        for(int time = 1;time<=n;time++){
-            for(int dish = time;dish<=n;dish++){
-                dp[time][dish] = dp[time-1][dish-1] + (arr[dish-1]*time);
-                maxVal = Math.max(dp[time][dish],maxVal);
+        for(int dish = 1;dish<=n;dish++){
+            for(int time = 1;time<=dish;time++){
+                dp[dish][time] = dp[dish-1][time-1]+arr[dish-1]*time;
+                maxVal = Math.max(maxVal,dp[dish][time]);
             }
         }
         return maxVal;
