009ed7d242022fb98651fe33665dd2b85d032eda
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-05 22:52:50 +0530

    Time: 4 ms (85.28%), Space: 43.4 MB (58.68%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,36 +1,30 @@
 class Solution {
     public String simplifyPath(String path) {
-        StringBuilder str = new StringBuilder();
-        String[] arr = path.split("/+");
         Stack<String> stack = new Stack<>();
-        for(String s:arr){
-            if(s.equals("..") && !stack.isEmpty()){
-                stack.pop();
-                if(!stack.isEmpty()){
+
+        String[] splits = path.split("/");
+        for(String split: splits) {
+            if(split.equals(".")) {
+                continue;
+            } else if(split.equals("..")) {
+                if(!stack.isEmpty()) {
                     stack.pop();
                 }
-                if(stack.isEmpty()){
-                    stack.push("/");
-                }
-            }else if(s.equals(".")){
+            } else if(split.length() == 0) {
                 continue;
-            }else{
-                
-                stack.push(s);
-                stack.push("/");
+            } else {
+                stack.push(split);
             }
-        }
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(stack);
 
-        Stack<String> st = new Stack<>();
-        while(!stack.isEmpty()){
-            st.push(stack.pop());
         }
-        while(!st.isEmpty()){
-            str.append(st.pop());
+
+        StringBuilder sb = new StringBuilder();
+        for(String dir: stack) {
+            sb.append("/");
+            sb.append(dir);
         }
-        if(str.length() == 1 || str.length() == 0) return "/";
-        return str.deleteCharAt(str.length()-1).toString();
+
+        return sb.length() > 0 ? sb.toString(): "/";
     }
 }
+
