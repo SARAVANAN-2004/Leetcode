3b51178ca10d5693055677b514993c6d192d0e7e
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-05 21:50:27 +0530

    Time: 15 ms (5.01%), Space: 44.8 MB (14.14%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String simplifyPath(String path) {
+        StringBuilder str = new StringBuilder();
+        String[] arr = path.split("/+");
+        Stack<String> stack = new Stack<>();
+        for(String s:arr){
+            if(s.equals("..") && !stack.isEmpty()){
+                stack.pop();
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    stack.push("/");
+                }
+            }else if(s.equals(".")){
+                continue;
+            }else{
+                
+                stack.push(s);
+                stack.push("/");
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(stack);
+
+        Stack<String> st = new Stack<>();
+        while(!stack.isEmpty()){
+            st.push(stack.pop());
+        }
+        while(!st.isEmpty()){
+            str.append(st.pop());
+        }
+        if(str.length() == 1 || str.length() == 0) return "/";
+        return str.deleteCharAt(str.length()-1).toString();
+    }
+}
