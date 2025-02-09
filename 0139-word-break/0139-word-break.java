142a5df5cacca81db0da8fc466e8200d4db6d8e5
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-09 12:47:00 +0530

    Time: 7 ms (52.63%), Space: 45.2 MB (9.04%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,69 +1,28 @@
-
-class Node{
-    Node[] child;
-    boolean flag;
-
-    public Node(){
-        child = new Node[26];
-        flag = false;
-    }
-}
-
-
-
-class Trie {
-    Node root;
-    public Trie() {
-        root = new Node();
-    }
-    
-    public void insert(String word) {
-        Node node = root;
-        for(char ch : word.toCharArray()){
-            int i = ch - 'a';
-            if(node.child[i] == null){
-                node.child[i] = new Node();
-            }
-            node = node.child[i];
-        }
-
-        node.flag = true;
-    }
-    
-    public boolean search(String word) {
-        Node node = root;
-        for(char ch:word.toCharArray()){
-            int i = ch-'a';
-            if(node.child[i] == null){
-                return false;
-            }
-            node = node.child[i];
-        }
-        return node.flag;
-    }
-
-}
-
-
 class Solution {
+    Set<String> set = new HashSet<>();
+    Map<Integer,Boolean> memo = new HashMap<>();
     public boolean wordBreak(String s, List<String> wordDict) {
-        Trie trie = new Trie();
-        for(String word:wordDict){
-            trie.insert(word);
+        for(String str:wordDict){
+            set.add(str);
         }
-        boolean dp[] = new boolean[s.length()+1];
-        dp[0] = true;
-
-        for(int i = 1;i<=s.length();i++){
-            for(int j = 0;j<i;j++){
-                if(dp[j] && trie.search(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
+        return solve(s,0);
+    }
+    boolean solve(String s,int start){
+        if(start == s.length()){
+            return true;
+        }
+        if(memo.containsKey(start)){
+            return memo.get(start);
+        }
+        for(int end = start+1;end <= s.length();end++){
+            if(set.contains(s.substring(start,end))){ 
+                if(solve(s,end)){
+                    memo.put(start,true);
+                    return true;
                 }
             }
         }
-        // System.out.println(Arrays.toString(dp));
-        return dp[s.length()];
+        memo.put(start,false);
+        return false;
     }
-
 }
