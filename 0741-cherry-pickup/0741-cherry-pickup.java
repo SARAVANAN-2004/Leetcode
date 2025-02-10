71b869f974a2a39a3df88fda108032de627ed784
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-10 13:32:56 +0530

    Time: 16 ms (56.49%), Space: 45 MB (64.93%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -11,11 +11,11 @@ class Solution {
                 Arrays.fill(j,-1);
             }
         }
-        int cheries  = solve(0,0,0,grid);
+        int cheries  = solve(0,0,0,0,grid);
         return Math.max(0,cheries);
     }
-    int solve(int r1,int r2,int c1,int[][] grid){
-        int c2 = r1+c1-r2;
+    int solve(int r1,int r2,int c1,int c2,int[][] grid){
+       
         if(r1 >= n || r2 >= n || c1 >= n  || c2 >= n || grid[r1][c1] == -1|| grid[r2][c2] == -1){
             return Integer.MIN_VALUE;
         }
@@ -29,10 +29,10 @@ class Solution {
             cherries += grid[r2][c2];
         }
 
-        int rr = solve(r1,r2,c1+1,grid);
-        int rd = solve(r1,r2+1,c1+1,grid);
-        int dr = solve(r1+1,r2,c1,grid);
-        int dd = solve(r1+1,r2+1,c1,grid);
+        int rr = solve(r1,r2,c1+1,c2+1,grid);
+        int rd = solve(r1,r2+1,c1+1,c2,grid);
+        int dr = solve(r1+1,r2,c1,c2+1,grid);
+        int dd = solve(r1+1,r2+1,c1,c2,grid);
 
         return dp[r1][r2][c1] = cherries + Math.max(Math.max(rr,rd),Math.max(dr,dd));
     }
