46997b779637bb89dd726a6814da802804234e15
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-14 16:55:14 +0530

    Time: 7 ms (29.19%), Space: 44.8 MB (52.28%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String s:tokens){
+            if(s.length()> 1){
+                stack.push(Integer.valueOf(s));
+                continue;
+            }
+            Character ch = s.charAt(0);
+            if(Character.isDigit(ch)){
+                stack.push((int)(ch - '0'));
+            }else{
+                // System.out.println(stack.size()+" "+stack);
+                int b = stack.pop() , a = stack.pop();
+                switch(ch){
+                    case '+':
+                        stack.push(a+b);
+                        break;
+                    case '-':
+                        stack.push(a-b);
+                        break;
+                    case '*':
+                        stack.push(a*b);
+                        break;
+                    case '/':
+                        stack.push(a/b);
+                        break;
+                }
+            }
+        }
+        return stack.peek();
+    }
+}
