23a3a5a88fb95fddccf5c36aa74f351ba675c7a2
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-18 16:45:51 +0530

    Time: 3 ms (38.28%), Space: 56.4 MB (31.71%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -4,20 +4,19 @@ class Solution {
         long sum = 0;
         long maxi = 0;
         int n = gas.length;
-        int[] arr = new int[n];
+        
         for(int i = 0;i<n;i++){
-            arr[i] = gas[i] - cost[i];
-            sum += arr[i];
-        }
-        if(sum < 0) return -1;
-        for(int i = 0;i<n;i++){
-            if(arr[i] > arr[i]+ maxi){
+            int curval = gas[i] - cost[i];
+            sum += curval;
+            if(curval > curval+ maxi){
                 st = i;
-                maxi = arr[i];
+                maxi = curval;
             }else{
-                maxi += arr[i];
+                maxi += curval;
             }
         }
+        if(sum < 0) return -1;
+        
         return st;
 
     }
