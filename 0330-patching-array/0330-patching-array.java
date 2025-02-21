b2a69fa944cfdbdac03a337bb27650b01e66f2a5
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-21 20:39:53 +0530

    Time: 0 ms (100%), Space: 44.1 MB (66.92%) - LeetHub

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -2,15 +2,24 @@ class Solution {
     public int minPatches(int[] nums, int n) {
         int cnt = 0;
         long till = 0;
-        for(int i = 0;till < n && i < nums.length;i++){
-            if(till > nums[i]-1){
-                till+=nums[i];
+        int i = 0;
+        // for(int i = 0;till < n && i < nums.length;i++){
+        //     if(till > nums[i]-1){
+        //         till+=nums[i];
+        //     }else{
+        //         while(till < n && till < nums[i]-1){
+        //             till += (till+1);
+        //             cnt++;
+        //         }
+        //         till += nums[i];
+        //     }
+        // }
+        while(i < nums.length && till < n){
+            if(till >= nums[i]-1){
+                till+=nums[i++];
             }else{
-                while(till < n && till < nums[i]-1){
-                    till += (till+1);
-                    cnt++;
-                }
-                till += nums[i];
+                till += (till+1);
+                cnt++;
             }
         }
         while(till < n){
