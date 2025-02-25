2292d47e634983cd42c724f6c6927ac4b1e4fbe7
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-25 14:40:23 +0530

    Time: 185 ms (5.1%), Space: 46.1 MB (52.55%) - LeetHub

--- 0887-minimum-cost-to-hire-k-workers/0887-minimum-cost-to-hire-k-workers.java
+++ 0887-minimum-cost-to-hire-k-workers/0887-minimum-cost-to-hire-k-workers.java
@@ -3,33 +3,31 @@ class Solution {
         int q;
         double wqr;
 
-        public worker(int q,double wqr){
+        worker(int q,double wqr){
             this.q = q;
             this.wqr = wqr;
         }
-
     }
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        List<worker> arr = new ArrayList<>();
         int n = wage.length;
+        List<worker> arr= new ArrayList<>();
         for(int i = 0;i<n;i++){
-            arr.add(new worker(quality[i],((double)wage[i] / quality[i])));
+            arr.add(new worker(quality[i],((double) wage[i] / quality[i])));
         }
-        Collections.sort(arr,(a,b)->Double.compare(a.wqr,b.wqr));
+        Collections.sort(arr,(a,b)-> Double.compare(a.wqr,b.wqr));
         double minCost = Double.MAX_VALUE;
         int total = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq =new PriorityQueue<>(Collections.reverseOrder());
         for(worker w:arr){
             total += w.q;
             pq.add(w.q);
-
+            
             if(pq.size() == k){
                 double curCost = total*w.wqr;
                 total -= pq.poll();
-                minCost  = Math.min(curCost,minCost);
+                minCost = Math.min(curCost,minCost);
             }
         }
-
         return minCost;
     }
 }
