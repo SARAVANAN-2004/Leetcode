6e12a096810a471e5adf2fcc5b3c526fff037bb4
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-02-26 21:38:22 +0530

    Time: 1 ms (99.79%), Space: 44.4 MB (44.31%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,15 +1,27 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int row = triangle.size();
-        for(int r = row-2;r>=0;r--){
-            for(int c = 0;c<triangle.get(r).size();c++){
-                int x = triangle.get(r+1).get(c);
-                int y = triangle.get(r+1).get(c+1);
-                int z = triangle.get(r).get(c);
-                triangle.get(r).set(c,Math.min(x,y)+z);
-                
-            }
+        if(triangle.size() == 1) return triangle.get(0).get(0);
+        int m = triangle.size();
+        int n = triangle.get(triangle.size()-1).size();
+        int [][] dp= new int[m][n];
+        for(int i=0; i<m; i++){
+            Arrays.fill(dp[i], -1);
         }
-        return triangle.get(0).get(0);
+        return findMinPath(triangle, 0, 0, dp);
+    }
+
+    public int findMinPath(List<List<Integer>> triangle, int index, int col, int[][] dp){
+        if(index == triangle.size()-1){
+            return triangle.get(index).get(col);
+        }
+        if(index >= triangle.size()|| col >= triangle.get(index).size()){
+            return (int) 1e9;
+        }
+
+        if(dp[index][col] != -1) return dp[index][col];
+
+        int sum1 = triangle.get(index).get(col) + findMinPath(triangle, index+1, col, dp);
+        int sum2 = triangle.get(index).get(col) + findMinPath(triangle, index+1, col+1, dp);
+        return dp[index][col]=Math.min(sum1, sum2);
     }
 }
