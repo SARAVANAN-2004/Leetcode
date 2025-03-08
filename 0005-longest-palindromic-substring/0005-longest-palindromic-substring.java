ff8bbb57528b0510fe332b17181c5b860bfef443
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-08 07:57:41 +0530

    Time: 119 ms (37.39%), Space: 55.9 MB (5.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        for(int i = 0;i<n;i++){
-            dp[i][i]  = true;
+        int[][] dp = new int[n+1][n+1];
+        if(n == 1) return s;
+        for(int i = 1;i<=n;i++){
+            dp[i][i] = 1;
         }
-        int[] idx = new int[2];
-        int max = 0;
+        int max = 1;
+        int[] idx = new int[]{0,0};
         for(int i = 0;i<n-1;i++){
             if(s.charAt(i) == s.charAt(i+1)){
-                dp[i][i+1] = true;
-                dp[i+1][i] = true;
-                max = 2;
-                idx[0] = i;
-                idx[1] = i+1;
-             }
+                dp[i][i+1] = 1;
+                dp[i+1][i] = 1;
+                
+                if(max == 1){
+                    idx[0] = i;
+                    idx[1] = i+1;
+                    max = 2;
+
+                }
+            }
         }
-        for(int r  = n-3;r>=0;r--){
+        
+        for(int r = n-3;r>=0;r--){
             for(int c = r+2;c<n;c++){
                 if(s.charAt(r) == s.charAt(c)){
-                    dp[r][c] = dp[r+1][c-1];
-                    if(dp[r][c]){
-                        if(max < c - r+1){
+                    if(dp[r+1][c-1] == 1){
+                        dp[r][c] = 1;
+                        if(dp[r][c] == 1 && max <= c-r+1){
                             max = c-r+1;
                             idx[0] = r;
                             idx[1] = c;
+                            
                         }
                     }
                 }
             }
         }
-        // for(boolean i[]:dp){
+        // for(int[] i :dp){
         //     System.out.println(Arrays.toString(i));
         // }
-        // System.out.println(max);
         return s.substring(idx[0],idx[1]+1);
     }
 }
