feba07e047550b0076fc178230b67764aa34cc34
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-10 17:46:38 +0530

    Time: 2 ms (24.7%), Space: 44.2 MB (52.65%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compress(char[] chars) {
+        int idx = 0;
+        int n = chars.length;
+        for(int i = 0;i<n;i++){
+            int cnt = 0;
+            int j = i;
+            while(j < n && chars[i] == chars[j]){
+                cnt++;
+                j++;
+            }
+            chars[idx++] = chars[i];
+            if(cnt == 1) continue;
+            else{
+               for(char ch:(""+cnt).toCharArray()) chars[idx++] = ch;
+               i = j-1;
+            }
+        }
+
+
+        return idx;
+    }
+}
