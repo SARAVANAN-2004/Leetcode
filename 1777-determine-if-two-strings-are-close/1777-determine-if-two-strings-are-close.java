b252386f513bb9825bc0c22f09f26f2a54fef065
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-18 10:58:57 +0530

    Time: 10 ms (84.8%), Space: 45.5 MB (82.03%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,42 +1,25 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()) return false;
-        Set<Character> set = new HashSet<>();
-        for (char c : word1.toCharArray()) {
-            set.add(c);
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(char ch:word1.toCharArray()){
+            freq1[ch-'a']++;
         }
-        for(char c:word2.toCharArray()){
-            if(!set.contains(c)) return false;
-        }
-        Map<Integer,Integer> m1 =solve(word1);
-        Map<Integer,Integer> m2 = solve(word2);
-        Set<Integer> s1 = m1.keySet();
-        Set<Integer> s2 = m2.keySet();
-        for(int i:s1){
-            if(!s2.contains(i) || m1.get(i) != m2.get(i)){
-                return false;
-            }
-        }for(int i:s2){
-            if(!s1.contains(i) || m1.get(i) != m2.get(i)){
-                return false;
-            }
+        for(char ch:word2.toCharArray()){
+            freq2[ch-'a']++;
         }
 
+        for(int i = 0;i<26;i++){
+            if((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)){
+                return false;
+            }
+        }
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        for(int i = 0;i<26;i++){
+            if(freq1[i] != freq2[i]) return false;
+        }
         return true;
-    }
-
-    Map<Integer,Integer> solve(String word1){
-        int[] arr = new int[26];
-        for(char ch :word1.toCharArray()){
-            arr[ch-'a']++;
-        }
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i:arr){
-            if(i != 0){
-                map.put(i,map.getOrDefault(i,0)+1);
-            }
-        }
-        return map;
         
     }
 }
