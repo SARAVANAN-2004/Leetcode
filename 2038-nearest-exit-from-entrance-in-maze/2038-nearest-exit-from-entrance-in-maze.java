5d0484cefeba2f7d0f9202dd792415a4827fa9e4
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-18 11:22:33 +0530

    Time: 12 ms (17.85%), Space: 46.1 MB (23.5%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -2,15 +2,16 @@ class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         Queue<int[]> queue = new LinkedList<>();
         int row = maze.length, col = maze[0].length;
-        // boolean[][] vis = new boolean[row][col];
+        boolean[][] vis = new boolean[row][col];
         queue.add(new int[]{entrance[0],entrance[1],0});
-        maze[entrance[0]][entrance[1]] = '+';
+        vis[entrance[0]][entrance[1]] = true;
         
         while(!queue.isEmpty()){
-            int r = queue.peek()[0];
-            int c = queue.peek()[1];
-            int step = queue.peek()[2];
-            queue.poll();
+            int[] cur = queue.poll();
+            int r = cur[0];
+            int c = cur[1];
+            int step = cur[2];
+           
             int[][] adj = new int[][]{
                 {-1,0},
                 {1,0},
@@ -20,9 +21,10 @@ class Solution {
             for(int i = 0;i<4;i++){
                 int curR = r + adj[i][0];
                 int curC = c + adj[i][1];
-                if(valid(curR,curC,row,col,maze)){
+                if(valid(curR,curC,row,col,maze) && !vis[curR][curC]){
+
                     if(curR == 0 || curC == 0 || curR == row-1 || curC == col-1) return step+1;
-                    maze[curR][curC] = '+';
+                    vis[curR][curC] = true;
                     queue.add(new int[]{curR,curC,step+1});
                 }
             }
