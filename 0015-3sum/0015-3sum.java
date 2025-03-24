ad7e2851611efef94c9f07a1fdd31c8a359e4ed2
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-24 19:56:18 +0530

    Time: 30 ms (67.68%), Space: 51.5 MB (59.16%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,29 +6,22 @@ class Solution {
         int n= nums.length;
         for(int k = 0;k<n-1;k++){
             if(k > 0 && nums[k] == nums[k-1]) continue;
-            int i = 0, j = n-1;
+            int i = k+1, j = n-1;
             int n1 = nums[k];
             while(i < j){
-                if(i == k) i++;
-                else if(j == k) j--;
-                else{
-                    int sum = nums[i]+nums[j]+n1;
-                    if(sum == 0){
-                        int max = Math.max(nums[i],Math.max(nums[j],n1));
-                        int min = Math.max(nums[i],Math.min(nums[j],n1));
-                        String key = min+" "+(sum - max-min)+" "+max;
-                        if(!set.contains(key)){
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                        }
-                        set.add(key);
-                        i++;
-                        j--;
-                    }else if(sum < 0){
-                        i++;
-                    }else{
-                        j--;
-                    }
+                int sum = nums[i]+nums[j]+n1;
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    i++;
+                    while(nums[i] == nums[i-1] && i < j) i++;
+                    j--;
+                    while(nums[j] == nums[j+1] && i < j) j--;
+                }else if(sum < 0){
+                    i++;
+                }else{
+                    j--;
                 }
+                
             }
         }
         return ans;
