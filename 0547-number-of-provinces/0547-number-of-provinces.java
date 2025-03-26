f0b9645ec150195a7fb2a453c8612994138ad290
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-26 20:43:33 +0530

    Time: 1 ms (89.06%), Space: 44.8 MB (99.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int findCircleNum(int[][] arr) {
-        int  cnt = 0;
-        int n = arr.length;
-        boolean[] vis = new boolean[n];
+    boolean[] vis;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        vis = new boolean[n];
+        int cnt = 0;
         for(int i = 0;i<n;i++){
             if(!vis[i]){
                 cnt++;
-                process(arr,n,vis,i);
+                process(isConnected,n,vis,i);
             }
         }
         return cnt;
     }
-    void process(int[][] arr,int n,boolean[] vis,int curRow){
-        vis[curRow] = true;
+
+    void process(int[][] arr,int n ,boolean[] vis,int cur){
+        vis[cur] = true;
         for(int i = 0;i<n;i++){
-            if(arr[curRow][i] == 1 && !vis[i]){
+            if(arr[cur][i] == 1 && !vis[i]){
                 process(arr,n,vis,i);
             }
         }
