4c8c7612557495c235f4daead26d01f811d1e237
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-30 09:20:44 +0530

    Time: 0 ms (100%), Space: 42.7 MB (10.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -16,18 +16,20 @@ class Solution {
             arr[i++] = number.get(ch);
         }
         if(arr.length == 0) return res;
-        solve(arr,"",0,arr.length);
+        solve(arr,new StringBuilder(),0,arr.length);
         return res;
     }
 
-    void solve(String[] arr, String temp,int i,int n){
+    void solve(String[] arr, StringBuilder temp,int i,int n){
         if(i == n){
-            res.add(temp);
+            res.add(temp.toString());
             return;
         }
         String s = arr[i];
         for(char ch:s.toCharArray()){
-            solve(arr,temp+ch,i+1,n);
+            temp.append(ch);
+            solve(arr,temp,i+1,n);
+            temp.deleteCharAt(temp.length()-1);
         }
     }
 }
