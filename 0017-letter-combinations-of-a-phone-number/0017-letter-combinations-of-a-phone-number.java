35f8c5a251baa1891a239c84ede72e52c8ecd79c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-30 09:18:26 +0530

    Time: 5 ms (30.21%), Space: 42.5 MB (17.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,33 +1,33 @@
 class Solution {
+    List<String> res = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        HashMap<String,String> number = new HashMap<>();
-        number.put("2","abc");
-        number.put("3","def");
-        number.put("4","ghi");
-        number.put("5","jkl");
-        number.put("6","mno");
-        number.put("7","pqrs");
-        number.put("8","tuv");
-        number.put("9","wxyz");
-        List<String> res = new ArrayList<>();
-        if(digits.isEmpty()){
-            return res;
+        HashMap<Character,String> number = new HashMap<>();
+        number.put('2',"abc");
+        number.put('3',"def");
+        number.put('4',"ghi");
+        number.put('5',"jkl");
+        number.put('6',"mno");
+        number.put('7',"pqrs");
+        number.put('8',"tuv");
+        number.put('9',"wxyz");
+        String[] arr = new String[digits.length()];
+        int i = 0;
+        for(char ch:digits.toCharArray()){
+            arr[i++] = number.get(ch);
         }
-        StringBuilder temp = new StringBuilder();
-        helper(digits,number,res,temp,0);
+        if(arr.length == 0) return res;
+        solve(arr,"",0,arr.length);
         return res;
     }
-    void helper(String digits,HashMap<String,String> number,List<String> res,StringBuilder temp,int curidx){
-        if(temp.length() == digits.length()){
-            res.add(temp.toString());
+
+    void solve(String[] arr, String temp,int i,int n){
+        if(i == n){
+            res.add(temp);
             return;
         }
-        String word = number.get(digits.charAt(curidx)+"");
-        for(int i = 0;i<word.length();i++){
-            char ch = word.charAt(i);
-            temp.append(ch);
-            helper(digits,number,res,temp,curidx+1);
-            temp.deleteCharAt(temp.length()-1);
+        String s = arr[i];
+        for(char ch:s.toCharArray()){
+            solve(arr,temp+ch,i+1,n);
         }
     }
 }
