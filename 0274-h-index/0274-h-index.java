ee562ac13f7c296a182190627ebc7d7b56731004
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-31 12:11:13 +0530

    Time: 3 ms (18.62%), Space: 41.9 MB (19.9%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int hIndex(int[] num) {
-        Arrays.sort(num);
-        int n = num.length;
-        int h = n-1,l = 0;
-        while(l <= h){
-            int mid = (h+l)/2;
-            
-            if(num[mid] == n - mid){
-                return num[mid];
-            }else if(num[mid] > n-mid){
-                h = mid-1;
-                
-            }else{
-                l = mid+1;
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] count = new int[n + 1];
+        
+        // Step 1: Fill the count array using a traditional for loop
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n) {
+                count[n]++; // Count citations greater than or equal to n
+            } else {
+                count[citations[i]]++;
             }
         }
-        return n-l;
+        System.out.println(Arrays.toString(count));
+        
+        // Step 2: Calculate the h-index
+        int totalPapers = 0;
+        for (int i = n; i >= 0; i--) {
+            totalPapers += count[i];
+            if (totalPapers >= i) {
+                return i; // Return h-index
+            }
+        }
+        
+        return 0; // In case no valid h-index is found
     }
 }
