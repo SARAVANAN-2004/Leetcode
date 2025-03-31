9349b778a3d39dc08b0ad1ad45a2728890a63a08
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-31 12:57:56 +0530

    Time: 45 ms (65.15%), Space: 66.7 MB (93.22%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,32 +1,30 @@
 class Solution {
+    int cnt = 0;
+    List<List<int[]>> adj;
+    boolean[] vis;
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> adj = new ArrayList<>();
+        adj = new ArrayList<>();
         for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        boolean[] vis = new boolean[n];
+        vis = new boolean[n];
         for(int i[]:connections){
             adj.get(i[0]).add(new int[]{i[1],1});
             adj.get(i[1]).add(new int[]{i[0],0});
         }
-        Queue<Integer> que = new LinkedList<>();
-        que.add(0);
-        vis[0] = true;
-        int cnt = 0;
-        while(!que.isEmpty()){
-            int cur = que.poll();
-            List<int[]> curAdj = adj.get(cur);
-            for(int[] a:curAdj){
-                if(!vis[a[0]]){
-                    cnt += a[1];
-                    vis[a[0]] = true;
-                    que.add(a[0]);
-                }
-
-            }
         
-        }
-
+        dfs(0);
         return cnt;
+    }
+    void dfs(int cur){
+        
+        vis[cur] = true;
+
+        for(int[] a:adj.get(cur)){
+            if(!vis[a[0]]){
+                cnt += a[1];
+                dfs(a[0]);
+            }
+        }
     }
 }
