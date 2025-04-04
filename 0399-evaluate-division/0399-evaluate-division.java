af796b5fc70a20241379b84441245dcf597c4a46
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-04-04 11:23:46 +0530

    Time: 2 ms (31.89%), Space: 42.4 MB (55.31%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -0,0 +1,59 @@
+class Solution {
+    class Pair{
+        String s;
+        double val;
+        public Pair(String s,double val){
+            this.s = s;
+            this.val = val;
+        }
+    }
+    Map<String,List<Pair>> map;
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        map =  new HashMap<>();
+        for(int i = 0;i<values.length;i++){
+            List<String> adj = equations.get(i);
+            double val = values[i];
+            Pair pair1 = new Pair(adj.get(1),val);
+            Pair pair2 = new Pair(adj.get(0),1/val);
+            map.computeIfAbsent(adj.get(0),k->new ArrayList<>()).add(pair1);
+            map.computeIfAbsent(adj.get(1),k->new ArrayList<>()).add(pair2);
+        }
+        // for(String s:map.keySet()){
+        //     System.out.println(s);
+        //     for(Pair pair: map.get(s)){
+        //         System.out.println(pair.s+" "+pair.val);
+        //     }
+        // }
+        double[] ans = new double[queries.size()];
+        for(int i = 0;i<queries.size();i++){
+            List<String> adj = queries.get(i);
+            ans[i] = solve(adj.get(0),adj.get(1));
+        }
+
+        return ans;
+    }
+
+    double solve(String src,String des){
+        if(!map.containsKey(src) || !map.containsKey(des)){
+            return -1;
+        }
+
+        Queue<Pair> que = new LinkedList<>();
+        Set<String> vis = new HashSet<>();
+        vis.add(src);
+        que.add(new Pair(src,1));
+        while(!que.isEmpty()){
+            Pair cur = que.poll();
+            if(cur.s.equals(des)) return cur.val;
+            for(Pair adj :map.get(cur.s)){
+                if(!vis.contains(adj.s)){
+                    vis.add(adj.s);
+                    que.add(new Pair(adj.s,cur.val * adj.val));
+                }
+            }
+        }
+
+        return -1;
+
+    }
+}
