c8890e4197018966f630ae215b4254f3c9a69071
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-04-20 01:39:12 +0530

    Time: 1 ms (40.74%), Space: 41.4 MB (27.07%) - LeetHub

--- 0770-couples-holding-hands/0770-couples-holding-hands.java
+++ 0770-couples-holding-hands/0770-couples-holding-hands.java
@@ -1,31 +1,38 @@
 class Solution {
+    Map<Integer,Integer> map;
     public int minSwapsCouples(int[] row) {
+        map  = new HashMap<>();
         int n = row.length;
-        int[] grp = new int[n];
-        for(int g = 0;g < n/2;g++){
-            grp[g*2] = g;
-            grp[g*2+1] = g;
+        // Read the elements into the array and store their indices in the map
+        for (int i = 0; i < n; i++) {
+            // row[i] = in.nextInt();
+            map.put(row[i], i);
         }
-        int swap = 0;
-        for(int i = 0;i<n;i+=2){
-            int fid = row[i];
-            int secid = row[i+1];
-            if(fid != (secid^1)){
-                if(grp[fid] != grp[secid]){
-                    swap++;
-                    union(grp,fid,secid);
-                }
+         int swaps = 0;
+
+        // Iterate through the array in steps of 2
+        for (int i = 0; i < n; i += 2) {
+            int first = row[i];
+            
+            // Determine the expected second element based on the first element
+            int second = first + (first % 2 == 0 ? 1 : -1);
+            
+            // If the next element is not the expected second element, perform a swap
+            if (row[i + 1] != second) {
+                swaps++;
+                swap(row, i + 1, map.get(second));
             }
         }
-        return swap;
+        return swaps;
     }
-    void union(int[] grp ,int fid,int secid){
-        int grpFid = grp[fid];
-        for(int i = 0;i<grp.length;i++){
-            if(grp[i] == grpFid){
-                grp[i] = grp[secid];
-            }
-        }
+    public  void swap(int[] row, int i, int j) {
+        int t = row[i];
+        row[i] = row[j];
+        row[j] = t;
+
+        // Update the map with the new indices of the swapped elements
+        map.put(row[i], i);
+        map.put(row[j], j);
     }
 
 
