401848f3af5711e3d9b82c3d86064ea0aee16f42
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-04-20 08:08:27 +0530

    Time: 113 ms (11.08%), Space: 68.9 MB (20%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
@@ -3,67 +3,61 @@ class Solution {
     boolean[] vis;
     int n;
     List<List<Integer>> adj;
+
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         n = edges.length;
         adj = new ArrayList<>();
-        for(int i = 0;i<n;i++){
+        
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
-            if(edges[i] != -1){
+            if (edges[i] != -1) {
                 adj.get(i).add(edges[i]);
             }
         }
 
-
-
         dis = new int[n];
         vis = new boolean[n];
-        Arrays.fill(dis,-1);
-        boolean cycle =  solve(edges,node1);
-        if(cycle) return -1;
-        int[] dist_1 =new int[n];
-        for(int i = 0;i<n;i++) dist_1[i] = dis[i];
-        Arrays.fill(dis,-1);
+        Arrays.fill(dis, -1);
+        bfs(node1);
+        int[] dist1 = dis; 
+        dis = new int[n];
         vis = new boolean[n];
-        cycle =  solve(edges,node2);
-        if(cycle) return -1;
-        int[] dist_2 =dis;
+        Arrays.fill(dis, -1);
+        bfs(node2);
+        int[] dist2 = dis.clone(); 
 
-        
-        int min = n;
-        int node = -1;
-        for(int i = 0;i<n;i++){
-            if(dist_1[i] != -1 && dist_2[i] != -1){
-            int max = Math.max(dist_1[i],dist_2[i]);
-            if(max < min) {
-                min = max;
-                node = i;
-            }
-            }
-        }
-        return node;
-        
-    }
 
-    boolean solve(int[] nums,int node){
-        if(node == -1) return false;
-        Queue<Integer> que = new LinkedList<>();
-        que.add(node);
-        vis[node] = true;
-        int d = 0;
-        while(!que.isEmpty()){
-            int size = que.size();
-            for(int i = 0;i<size;i++){
-                int cur = que.poll();
-                dis[cur] = d;
-                for(int j :adj.get(cur)){
-                    if(!vis[j]){
-                        vis[j] = true;
-                        que.add(j);
-                    }
+        int minDist = Integer.MAX_VALUE;
+        int ans = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int max = Math.max(dist1[i], dist2[i]);
+                if (max < minDist) {
+                    minDist = max;
+                    ans = i;
                 }
             }
-            d++;
         }
-        return false;
+
+        return ans;
+    }
+
+    void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        vis[start] = true;
+        dis[start] = 0;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int neighbor : adj.get(cur)) {
+                if (!vis[neighbor]) {
+                    vis[neighbor] = true;
+                    dis[neighbor] = dis[cur] + 1;
+                    queue.add(neighbor);
+                }
+            }
+        }
     }
 }
