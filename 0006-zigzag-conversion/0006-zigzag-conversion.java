908d1c8b17e72034c2874b8aba1df7c64025d2c0
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-05-24 21:10:27 +0530

    Time: 7 ms (31.87%), Space: 45.4 MB (38.45%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,34 +1,28 @@
 class Solution {
     public String convert(String s, int row) {
-        List<List<Integer>> arr = new ArrayList<>();
-        if(row > s.length() || row == 1){
-            return s;
-        }
+        List<List<Character>> arr = new ArrayList<>();
         for(int i = 0;i<row;i++){
             arr.add(new ArrayList<>());
         }
-
-        StringBuilder ans = new StringBuilder();
-        fill(arr,row,s.length());
-        for(int i = 0;i<row;i++){
-            for(int j :arr.get(i)){
-                ans.append(s.charAt(j));
+        int i = -1;
+        int op = 1;
+        if(row == 1) return s;
+         for(char ch :s.toCharArray()){
+            i+=op;
+            if(i == row){
+                i-=2;
+                op = -1;
+            }else if(i == -1){
+                i+=2;
+                op = 1;
             }
+            // System.out.println(i+" ");
+            arr.get(i).add(ch);
         }
-        return ans.toString();
-    }
-    public static void fill(List<List<Integer>> arr,int row,int n){
-        int rdir = 1;
-        int r = -1;
-        for(int i = 0;i<n;i++){
-            r += rdir;
-            if(r>=row-1){
-                rdir = -1;     
-            }
-            if( r <= 0){
-                rdir = 1;
-            }
-           arr.get(r).add(i);
-        }
+       StringBuilder sb = new StringBuilder();
+       for(List<Character> a:arr){
+        for(char ch:a) sb.append(ch);
+       }
+       return sb.toString();
     }
 }
