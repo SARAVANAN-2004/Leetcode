bf0a9a6c93b67ceece9f63a2221c355b0d4e73de
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-05-26 22:49:57 +0530

    Time: 1 ms (90.4%), Space: 45 MB (57.86%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,33 +1,21 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        int prev = 1;
+        int suff = 1;
+        int maxi = nums[0];
         int n = nums.length;
-        int[] prefix = solve(nums);
-        int i = 0, j = n-1;
-        while(i < j){
-            int temp = nums[i];
-            nums[i++] = nums[j];
-            nums[j--] = temp;
-        }
-        int[] suffix = solve(nums);
-        int maxi = prefix[0];
-        for( i = 0;i<n;i++){
-            maxi = Math.max(maxi,Math.max(prefix[i],suffix[i]));
+        for(int i = 0;i<n;i++){
+            if(prev == 0){
+                prev = 1;
+            }
+            if(suff == 0){
+                suff = 1;
+            }
+            suff *= nums[n-i-1];
+            prev *= nums[i];
+
+            maxi = Math.max(maxi,Math.max(prev,suff));
         }
         return maxi;
-    }
-    int[] solve(int[] nums){
-        int n = nums.length;
-        int[] arr = new int[n];
-        int prev = 1;
-        for(int i = 0;i<n;i++){
-            if(nums[i] == 0){
-                arr[i] = 0;
-                prev = 1;
-            }else{
-                prev *= nums[i];
-                arr[i] = prev;
-            }
-        }
-        return arr;
     }
 }
