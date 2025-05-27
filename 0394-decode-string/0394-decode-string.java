c3a29d2963629d54e228ace15483c00193219057
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-05-27 23:20:51 +0530

    Time: 1 ms (68.6%), Space: 41.6 MB (68.25%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,61 +1,30 @@
 class Solution {
-    class obj{
-        int n = 0;
-        String s = "";
-        boolean num,bracket;
-        
-        public obj(int n,String s,boolean num,boolean bracket){
-            this.n = n;
-            this.s = s;
-            this.bracket = bracket;
-            this.num = num;
-        }
-    }
     public String decodeString(String s) {
-        Stack<obj> stk = new Stack<>();
-        
-        
-        for(char ch: s.toCharArray()){
-           if(stk.isEmpty() || ch != ']'){
-                
-                if(Character.isDigit(ch)){
-                    int n = Integer.valueOf(ch-'0');
-                    
-                    if(!stk.isEmpty() && stk.peek().num){
-                        stk.peek().n = stk.peek().n * 10+n;
-                    }else{
-                        stk.push(new obj(n,"",true,false));
-                    }
-                }
-                else if(ch == '['){
-                    stk.push(new obj(0,"[",false,true));
-                }else{
-                    if(!stk.isEmpty() && !stk.peek().num  &&  !stk.peek().bracket){
-                        stk.peek().s += ch;
-                    }else{
-                        stk.push(new obj(0,ch+"",false,false));
-                    }
-                }
-
-           }else{
-                String a = stk.pop().s;
-                stk.pop();
-                int n = stk.pop().n;
-                StringBuilder sb = new StringBuilder();
-                for(int i = 0;i<n;i++){
-                    sb.append(a);
-                }
-                if(!stk.isEmpty() && !stk.peek().num  &&  !stk.peek().bracket){
-                    stk.peek().s += sb.toString();
-                }else{
-                    stk.push(new obj(0,sb.toString(),false,false));
-                }
-
-           }
+        Stack<Integer> numberst = new Stack<>();
+        Stack<StringBuilder> charst = new Stack<>();
+        StringBuilder curr = new StringBuilder();
+        int number = 0;
+        for(char c : s.toCharArray()){
+         
+          if(Character.isDigit(c)){
+             number = number *10 + (c - '0');
+          }else if(c == '['){
+             numberst.add(number);
+             charst.add(curr);
+             curr = new StringBuilder();
+             number = 0;
+          }else if(c == ']'){
+            int count = numberst.pop();
+            StringBuilder temp = charst.pop();
+            while(count-- > 0){
+                temp.append(curr);
+            }
+            curr = temp;
+          }else{
+            curr.append(c);
+          }
+       
         }
-        // for(obj o:stk){
-        //     System.out.println(o.n+" "+o.s+" "+o.num+" "+o.bracket);
-        // }
-        return stk.peek().s;
+        return curr.toString();
     }
 }
