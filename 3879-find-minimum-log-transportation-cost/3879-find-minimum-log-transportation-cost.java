a27788881d3c6f1618d8ee96328f339cc91df0b1
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-05-30 08:25:04 +0530

    Time: 2 ms (11.28%), Space: 41 MB (49.26%) - LeetHub

---
+++ 3879-find-minimum-log-transportation-cost/3879-find-minimum-log-transportation-cost.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long minCuttingCost(int n, int m, int k) {
+        long cost = 0;
+        if(n >k){
+           cost = (long)(n - k)*k;
+           return cost;
+        }
+        else if(m > k){
+            cost = (long)(m - k)*k;
+           System.out.println(cost);
+           return cost;
+        }
+        return 0;
+        
+    }
+    
+    
+    
+}
