948529df516bf059b3e2cfac70d1fb41227e2692
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-06-02 13:39:09 +0530

    Time: 3 ms (84.79%), Space: 45.7 MB (70.42%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] dp = new int[n];
+    public int candy(int[] rat) {
+        int n = rat.length;
+        int[] dp  = new int[n];
         Arrays.fill(dp,1);
-        for(int i = 0;i<n-1;i++){
-            if(ratings[i] < ratings[i+1]){
-                dp[i+1] = Math.max(dp[i+1],dp[i]+1);
+        for(int i = 1;i<n;i++){
+            if(rat[i-1] < rat[i]){
+                dp[i] = dp[i-1]+1;
             }
         }
-        for(int i = n-1;i>0;i--){
-            if(ratings[i] < ratings[i-1]){
-                dp[i-1] = Math.max(dp[i-1],dp[i]+1);
+        for(int i = n-2;i>=0;i--){
+            if(rat[i] > rat[i+1] && dp[i] <= dp[i+1]){
+                dp[i] = Math.max(dp[i],dp[i+1])+1;
             }
         }
-
-        return Arrays.stream(dp).sum();
+        // System.out.println(Arrays.toString(dp));
+        int sum = 0;
+        for(int i:dp) sum+=i;
+        return sum;
     }
 }
