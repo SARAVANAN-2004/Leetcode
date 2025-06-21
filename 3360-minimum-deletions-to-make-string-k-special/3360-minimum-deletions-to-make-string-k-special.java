89e527a64a9abf9620faff90cbb6a7395cccaeff
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-06-21 09:46:27 +0530

    Time: 6 ms (51.06%), Space: 45.2 MB (97.87%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] map = new int[26];
+        int cnt = 0;
+        for(char ch:word.toCharArray()){
+            if(map[ch-'a'] == 0){
+                cnt++;
+            }
+            map[ch-'a']++;
+        }
+        int[] arr = new int[cnt];
+        int i = 0;
+        for(int j = 0;j<26;j++){
+            if(map[j] > 0){
+                arr[i++] = map[j];
+            }
+        }
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for(i = 0;i<cnt;i++){
+            int ch = arr[i];
+            int cur = 0;
+            for(int j = 0;j<cnt;j++){
+                if(i == j){
+                    continue;
+                }else{
+                   if(arr[j] < ch){
+                        cur += arr[j];
+                   }else if(arr[j] > ch + k){
+                        cur += arr[j] - ch - k;
+                   }
+                }
+            }
+            min = Math.min(min,cur);
+        }
+        return min;
+        
+    }
+
+
+}
