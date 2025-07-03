36bc4d7e4067fe3368eb3917c223eda89de450b6
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-07-03 14:20:24 +0530

    Time: 12 ms (16.13%), Space: 42.4 MB (25.81%) - LeetHub

---
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int minFlips(int[][] mat) {
+        Queue<Integer> que = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+        String  s = "";
+        int row = mat.length;
+        int col = mat[0].length;
+        for(int r = 0;r<row;r++){
+            for(int c = 0;c<col;c++){
+                s += mat[r][c];
+            }
+        }
+        que.add(Integer.parseInt(s,2));
+        set.add(Integer.parseInt(s,2));
+        // System.out.println(set);
+        int step = 0;
+
+        int [][] adj = new int[][]{
+            {0,-1},{0,1},{1,0},{-1,0},{0,0}
+        };
+        while(!que.isEmpty()){
+            int size = que.size();
+            for(int i  = 0;i<size;i++){
+                int cur = que.poll();
+                if(cur == 0) return step;
+                for(int x = 0;x<row * col;x++){
+                    int r = x / col, c = x%col;
+                    int temp = cur;
+                    for(int j = 0;j<5;j++){
+                        
+                        int ar = r+adj[j][0] , ac= c + adj[j][1];
+                        if(ar < 0 || ac < 0 || ar >= row || ac >= col) continue;
+                        int k = ar * col + ac;
+                        temp = temp ^ (1 << k);
+                        
+                    }
+                    if(!set.contains(temp)){
+                        que.add(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+            // System.out.println(que);
+            step++;
+        }
+
+        return -1;
+    }
+}
