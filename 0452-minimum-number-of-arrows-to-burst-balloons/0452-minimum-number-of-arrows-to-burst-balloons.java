cfd407acf7c095497ed25c8000f7d300f1c9ef6d
SARAVANAN G <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-07-23 10:50:01 +0530

    Time: 52 ms (81.43%), Space: 69.3 MB (27.8%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,37 +1,19 @@
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b)->{
-            if(a[0] == b[0]){
-                return a[1]-b[1];
-            }else{
-                return a[0] - b[0];
+    public int findMinArrowShots(int[][] pairs) {
+             if(pairs == null){
+            return 0;
+        }
+        int c = 1;
+         Arrays.sort(pairs, (p1, p2) -> Integer.compare(p1[1], p2[1]));
+
+        int prev = pairs[0][1];
+
+        for(int i=1; i<pairs.length; i++){
+            if(prev <  pairs[i][0]){
+                c++;
+                prev = pairs[i][1];
             }
         }
-        );
-        // for(int i[]:points){
-        // System.out.print(Arrays.toString(i)+" ");
-        // }
-        int arrow = 0;
-        int i = 0;
-        int n = points.length;
-        while(i < n){
-            int st = points[i][0];
-            int en = points[i][1];
-            i++;
-            arrow++;
-            
-            while(i < n){
-                int st1 = points[i][0];
-                int en1 = points[i][1];
-                if(st1 >= st && st1<=en){
-                    st = Math.max(st1,st);
-                    en = Math.min(en1,en);
-                    i++;
-                }else{
-                    break;
-                }
-            }
-        }
-        return arrow;
+        return c;
     }
 }
